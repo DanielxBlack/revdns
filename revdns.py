420cda5bd5f0e6b409e53a6c57d695dd216bd0ed
Daniel <DanielxBlack@users.noreply.github.com>
2021-12-19 21:40:26 -0600

    Update revdns.py

--- revdns.py
+++ revdns.py
@@ -4,7 +4,7 @@ import socket
 import ipaddress
 import os
 
-#clear terminal and display banner
+# Clear terminal and display banner
 os.system("clear")
 
 print(" _____            _____  _   _  _____ ")
@@ -27,7 +27,7 @@ IPaddy = input("Enter an IP or range: ")
 # User the ipaddress module to convert \24 to all 256 valid IPs.
 netWerk = ipaddress.ip_network(IPaddy)
 
-# for loop to do reverse dns lookup and print hostname.
+# For loop to do reverse dns lookup and print hostname.
 for IPs in netWerk.hosts():
     try:
         IPs = str(IPs)
@@ -35,10 +35,10 @@ for IPs in netWerk.hosts():
         result = dns_lookup[0]
         print(IPs + " resolves to " + result + ".")
 
-    # if no response, pass.
+    # If no response, pass.
     except socket.herror:
         pass
 
 
-# printing an error message for each non-resolved IP can get cluttered.
+# Printing an error message for each non-resolved IP can get cluttered.
 print("If you got no results, our reverse DNS lookups went unanswered.")
